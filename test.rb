945ed79ef047553606091dd4ad263d4a45c1e406
Anita Kuno <ay.kuno@gmail.com>
2009-01-26 13:58:34 -0500

    added test.rb, edited views/index.erb

---
+++ test.rb
@@ -0,0 +1,13 @@
+require 'test/unit'
+
+class Sample_Test < Test::Unit::TestCase
+  #def setup
+  #end
+
+  #def teardown
+  #end
+
+  def test_fail
+    assert(false, 'Assertion was false.')
+  end
+end
