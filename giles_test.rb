7de351f4a286df052916d30d6ff9480af52c15d2
anita <ay.kuno@gmail.com>
2009-01-31 22:11:55 -0500

    renamed giles_innocent.rb to giles_test.rb and created classes

---
+++ giles_test.rb
@@ -0,0 +1,61 @@
+require 'test/unit'
+
+  class Cops(*args)
+    def arrest(*args)
+      true
+    end
+  end
+
+  class Giles(*args)
+    def working
+      true
+    end
+    def tweets
+      true
+    end
+    def locates(*args)
+      true
+    end
+    def kill(*args)
+      true
+    end
+  end
+
+  class Child
+    def scream
+      true
+    end
+  end
+
+class Giles_Test < Test::Unit::TestCase
+
+  def test_setting
+    assert giles.working
+  end
+
+  def test_scream
+    assert child.scream
+  end
+
+  def test_frustration
+    assert giles.tweets
+  end
+
+  def test_radar
+    assert giles.locates(child)
+  end
+
+  def test_death
+    assert giles.kill(child)
+  end
+
+  def test_arrest
+    assert cops.arrest(giles)
+  end
+
+  def test_incarceration
+    assert giles.tweets
+  end
+
+end
+
